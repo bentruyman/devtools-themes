17d505b2220e741ae140e6bb33415f608c9b3219
Ben Truyman <bentruyman@gmail.com>
2012-10-18 21:29:59 -0500

    The name "Tomorrow Night" implies that it is a dark theme.

--- grunt.js
+++ grunt.js
@@ -7,12 +7,12 @@ module.exports = function (grunt) {
     clean: ['Custom.css'],
     watch: {
       files: ['Custom.less', 'themes/**/*.less'],
-      tasks: 'build:tomorrow-night-dark'
+      tasks: 'build:tomorrow-night'
     }
   });
   
   // Default task.
-  grunt.registerTask('default', 'build:tomorrow-night-dark');
+  grunt.registerTask('default', 'build:tomorrow-night');
   
   // Load NPM tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
