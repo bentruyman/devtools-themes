f4d16bf274e4c5de5e64353aaf7cff83940d8bcd
Ben Truyman <bentruyman@gmail.com>
2012-11-27 09:58:40 -0600

    Parameterized default theme built with grunt

--- grunt.js
+++ grunt.js
@@ -1,23 +1,26 @@
 module.exports = function (grunt) {
-  
+
   'use strict';
-  
+
+  // defaults
+  var defaultTheme = 'monokai-dark';
+
   // Project configuration.
   grunt.initConfig({
     clean: ['Custom.css'],
     watch: {
       files: ['Custom.less', 'themes/**/*.less'],
-      tasks: 'build:tomorrow-night'
+      tasks: 'build:' + defaultTheme
     }
   });
-  
+
   // Default task.
-  grunt.registerTask('default', 'build:tomorrow-night');
-  
+  grunt.registerTask('default', 'build:' + defaultTheme);
+
   // Load NPM tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
-  
+
   // Load local tasks.
   grunt.loadTasks('tasks');
-  
+
 };
